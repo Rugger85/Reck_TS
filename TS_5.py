34a06ba67cf0ecf9b51eb5a9550f293900cdfa8a
Rugger85 <123190705+Rugger85@users.noreply.github.com>
2024-09-27 18:42:34 +0500

    Update TS_5.py

--- TS_5.py
+++ TS_5.py
@@ -31,7 +31,8 @@ hide_streamlit_style = """
 
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 # change folder
-users = pd.read_excel("https://github.com/Rugger85/Reck_TS/blob/main/users_db.xlsx")
+url = "https://raw.githubusercontent.com/Rugger85/Reck_TS/main/users_db.xlsx"
+users = pd.read_excel(url)
 # change folder
 clients = pd.read_excel("clients.xlsx", sheet_name='Client', dtype=str)
 # change folder
